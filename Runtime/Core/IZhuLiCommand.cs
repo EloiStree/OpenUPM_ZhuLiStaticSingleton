89564667f5e0a9611cc438548de002611a652460
Éloi Strée <eloistree@gmail.com>
2023-07-30 01:37:56 +0200

    save

--- Runtime/Core/IZhuLiCommand.cs
+++ Runtime/Core/IZhuLiCommand.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 /// <summary>
 /// If the object inherit of this interface, it must be a struct or a class with just the information on what to do but no logic behind.
 /// ZhuLi (the classes behind the static instance) will know how to do the thing.
 /// </summary>
 public interface IZhuLiCommand { }
+
+[System.Serializable]
+public class IZhuLiCommandEvent : UnityEvent<IZhuLiCommand> { }
--- Runtime/Debug/DebugStaticZhuLiReceiverMono.cs
+++ Runtime/Debug/DebugStaticZhuLiReceiverMono.cs
@@ -3,13 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugZhuLiReceiverMono : MonoBehaviour
+public class DebugStaticZhuLiReceiverMono : MonoBehaviour
 {
     public string m_typeReceived;
     [TextArea(2,8)]
     public string m_receivedThingToDo;
-  
-    public void DebugTheThing(IZhuLiCommand theThingToDo)
+    public void OnEnable()
+    {
+        ZhuLi.AddListener(DebugTheThing);
+    }
+
+    public void OnDisable()
+    {
+        ZhuLi.RemoveListener(DebugTheThing);
+    }
+
+    private void DebugTheThing(IZhuLiCommand theThingToDo)
     {
         m_receivedThingToDo= JsonUtility.ToJson(theThingToDo);
         m_typeReceived = theThingToDo.GetType().ToString();
